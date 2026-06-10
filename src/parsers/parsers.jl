c07e831f673088be07a39008c9ecf78532f6175c
Franz Scharnreitner <fscharnreitner@gmail.com>
2026-06-10 17:15:25 +0200

    cleanup

--- src/parsers/parsers.jl
+++ src/parsers/parsers.jl
@@ -63,6 +63,80 @@ end
 Streams line-by-line using byte tokens and directly compiles a SparseMatrixCSC.
 Bypasses intermediate dense grid allocations completely.
 """
+@inline function is_delim_or_newline(b::UInt8, delim_byte::UInt8)
+    return b == delim_byte || b == 0x0a || b == 0x0d
+end
+
+@inline function try_skip_zero(raw_bytes::Vector{UInt8}, pos::Int, len::Int, delim_byte::UInt8)
+    if pos <= len && raw_bytes[pos] == 0x30
+        if pos == len || is_delim_or_newline(raw_bytes[pos + 1], delim_byte)
+            return 1
+        elseif pos + 2 <= len && raw_bytes[pos + 1] == 0x2e && raw_bytes[pos + 2] == 0x30 && (pos + 2 == len || is_delim_or_newline(raw_bytes[pos + 3], delim_byte))
+            return 3
+        end
+    end
+    return 0
+end
+
+function parse_chunk_range!(
+        I::Vector{Int}, J::Vector{Int}, V::Vector{Float64},
+        raw_bytes::Vector{UInt8}, len::Int, row_starts::Vector{Int},
+        c_start::Int, c_end::Int, ncols::Int, delim_byte::UInt8, opts::Parsers.Options
+    )
+    idx = 1
+    max_len = length(I)
+
+    for i in c_start:c_end
+        pos = row_starts[i]
+        for j in 1:ncols
+            # Skip delimiters
+            while pos <= len && is_delim_or_newline(raw_bytes[pos], delim_byte)
+                pos += 1
+            end
+
+            if pos > len
+                break
+            end
+
+            skip_len = try_skip_zero(raw_bytes, pos, len, delim_byte)
+            if skip_len > 0
+                pos += skip_len
+                continue
+            end
+
+            res = Parsers.xparse(Float64, raw_bytes, pos, len, opts)
+            if Parsers.ok(res.code)
+                val = res.val
+                if val != 0.0
+                    if idx > max_len
+                        max_len = max(10, max_len * 2)
+                        resize!(I, max_len)
+                        resize!(J, max_len)
+                        resize!(V, max_len)
+                    end
+                    @inbounds I[idx] = i
+                    @inbounds J[idx] = j
+                    @inbounds V[idx] = val
+                    idx += 1
+                end
+                pos += res.tlen
+            else
+                pos += 1
+            end
+        end
+    end
+
+    resize!(I, idx - 1)
+    resize!(J, idx - 1)
+    resize!(V, idx - 1)
+
+    return I, J, V
+end
+
+abstract type ParsingStrategy end
+struct Serial <: ParsingStrategy end
+struct Parallel <: ParsingStrategy end
+
 function custom_gloria_sparse_parser(raw_bytes::Vector{UInt8}, nrows::Int, ncols::Int; delim::Char = ',')
     if nrows < 0 || ncols < 0
         throw(ArgumentError("Dimensions must be non-negative"))
@@ -73,71 +147,30 @@ function custom_gloria_sparse_parser(raw_bytes::Vector{UInt8}, nrows::Int, ncols
     actual_rows = min(nrows, length(row_starts))
 
     n_threads = Threads.nthreads()
+    strategy = (n_threads <= 1 || actual_rows < 20) ? Serial() : Parallel()
 
-    if n_threads <= 1 || actual_rows < 20
-        # Coordinate collection vectors for sparse matrix generation
-        I = Int[]
-        J = Int[]
-        V = Float64[]
+    return custom_gloria_sparse_parser(strategy, raw_bytes, len, row_starts, actual_rows, nrows, ncols, delim, n_threads)
+end
 
-        # Pre-allocate using estimated ~15% matrix density to block array resizing overhead
-        est_nnz = (actual_rows * ncols) ÷ 5
-        sizehint!(I, est_nnz)
-        sizehint!(J, est_nnz)
-        sizehint!(V, est_nnz)
+function custom_gloria_sparse_parser(::Serial, raw_bytes::Vector{UInt8}, len::Int, row_starts::Vector{Int}, actual_rows::Int, nrows::Int, ncols::Int, delim::Char, n_threads::Int)
+    delim_byte = UInt8(delim)
+    opts = Parsers.Options(delim = delim)
 
-        opts = Parsers.Options(delim=delim)
-        delim_byte = UInt8(delim)
+    est_nnz = (actual_rows * ncols) ÷ 5
+    I = Vector{Int}(undef, est_nnz)
+    J = Vector{Int}(undef, est_nnz)
+    V = Vector{Float64}(undef, est_nnz)
 
-        for i in 1:actual_rows
-            pos = row_starts[i]
+    parse_chunk_range!(I, J, V, raw_bytes, len, row_starts, 1, actual_rows, ncols, delim_byte, opts)
 
-            for j in 1:ncols
-                # Allocation-free delimiter skipping
-                while pos <= len && (
-                        raw_bytes[pos] == delim_byte ||
-                            raw_bytes[pos] == 0x0a || raw_bytes[pos] == 0x0d
-                    )
-                    pos += 1
-                end
+    @info "Finished parsing matrix into highly compressed sparse format"
+    return construct_sparse_from_row_major(I, J, V, nrows, ncols)
+end
 
-                if pos > len
-                    break
-                end
+function custom_gloria_sparse_parser(::Parallel, raw_bytes::Vector{UInt8}, len::Int, row_starts::Vector{Int}, actual_rows::Int, nrows::Int, ncols::Int, delim::Char, n_threads::Int)
+    delim_byte = UInt8(delim)
+    opts = Parsers.Options(delim = delim)
 
-                if pos <= len && raw_bytes[pos] == 0x30
-                    if pos == len || raw_bytes[pos + 1] == delim_byte || raw_bytes[pos + 1] == 0x0a || raw_bytes[pos + 1] == 0x0d
-                        pos += 1
-                        continue
-                    elseif pos + 2 <= len && raw_bytes[pos + 1] == 0x2e && raw_bytes[pos + 2] == 0x30 && (pos + 2 == len || raw_bytes[pos + 3] == delim_byte || raw_bytes[pos + 3] == 0x0a || raw_bytes[pos + 3] == 0x0d)
-                        pos += 3
-                        continue
-                    end
-                end
-
-                res = Parsers.xparse(Float64, raw_bytes, pos, len, opts)
-
-                if Parsers.ok(res.code)
-                    val = res.val
-                    if val != 0.0  # ONLY keep true non-zero coefficients
-                        push!(I, i)
-                        push!(J, j)
-                        push!(V, val)
-                    end
-                    pos += res.tlen
-                else
-                    pos += 1
-                end
-            end
-        end
-
-        @info "Finished parsing matrix into highly compressed sparse format"
-        return construct_sparse_from_row_major(I, J, V, nrows, ncols)
-    end
-
-    # Multithreaded parsing
-    # Spawning a small multiple of n_threads (e.g., 4 * n_threads) allows Julia's work-stealing scheduler
-    # to dynamically balance the load across threads if some chunks take longer to parse than others.
     n_chunks = min(actual_rows, 4 * n_threads)
     chunks = Vector{Tuple{Int, Int}}(undef, n_chunks)
     chunk_size = div(actual_rows, n_chunks)
@@ -153,79 +186,36 @@ function custom_gloria_sparse_parser(raw_bytes::Vector{UInt8}, nrows::Int, ncols
         start_idx = end_idx + 1
     end
 
-    tasks = Vector{Task}(undef, n_chunks)
-    for t in 1:n_chunks
-        tasks[t] = Threads.@spawn begin
-            c_start, c_end = chunks[t]
-            chunk_rows = c_end - c_start + 1
-
-            local_I = Int[]
-            local_J = Int[]
-            local_V = Float64[]
-
-            if chunk_rows > 0
-                est_nnz_t = (chunk_rows * ncols) ÷ 5
-                sizehint!(local_I, est_nnz_t)
-                sizehint!(local_J, est_nnz_t)
-                sizehint!(local_V, est_nnz_t)
-
-                opts = Parsers.Options(delim=delim)
-                delim_byte = UInt8(delim)
-
-                for i in c_start:c_end
-                    pos = row_starts[i]
-                    for j in 1:ncols
-                        while pos <= len && (
-                                raw_bytes[pos] == delim_byte ||
-                                    raw_bytes[pos] == 0x0a || raw_bytes[pos] == 0x0d
-                            )
-                            pos += 1
-                        end
-
-                        if pos > len
-                            break
-                        end
-
-                        if pos <= len && raw_bytes[pos] == 0x30
-                            if pos == len || raw_bytes[pos + 1] == delim_byte || raw_bytes[pos + 1] == 0x0a || raw_bytes[pos + 1] == 0x0d
-                                pos += 1
-                                continue
-                            elseif pos + 2 <= len && raw_bytes[pos + 1] == 0x2e && raw_bytes[pos + 2] == 0x30 && (pos + 2 == len || raw_bytes[pos + 3] == delim_byte || raw_bytes[pos + 3] == 0x0a || raw_bytes[pos + 3] == 0x0d)
-                                pos += 3
-                                continue
-                            end
-                        end
-
-                        res = Parsers.xparse(Float64, raw_bytes, pos, len, opts)
-
-                        if Parsers.ok(res.code)
-                            val = res.val
-                            if val != 0.0
-                                push!(local_I, i)
-                                push!(local_J, j)
-                                push!(local_V, val)
-                            end
-                            pos += res.tlen
-                        else
-                            pos += 1
-                        end
-                    end
-                end
-            end
-
-            (local_I, local_J, local_V)
-        end
-    end
-
     thread_I = Vector{Vector{Int}}(undef, n_chunks)
     thread_J = Vector{Vector{Int}}(undef, n_chunks)
     thread_V = Vector{Vector{Float64}}(undef, n_chunks)
 
     for t in 1:n_chunks
-        res_t = fetch(tasks[t])::Tuple{Vector{Int}, Vector{Int}, Vector{Float64}}
-        thread_I[t] = res_t[1]
-        thread_J[t] = res_t[2]
-        thread_V[t] = res_t[3]
+        c_start, c_end = chunks[t]
+        chunk_rows = c_end - c_start + 1
+        est_nnz_t = (chunk_rows * ncols) ÷ 5
+        thread_I[t] = Vector{Int}(undef, est_nnz_t)
+        thread_J[t] = Vector{Int}(undef, est_nnz_t)
+        thread_V[t] = Vector{Float64}(undef, est_nnz_t)
+    end
+
+    tasks = Vector{Task}(undef, n_chunks)
+    for t in 1:n_chunks
+        tasks[t] = Threads.@spawn begin
+            c_start, c_end = chunks[t]
+            chunk_rows = c_end - c_start + 1
+            if chunk_rows > 0
+                parse_chunk_range!(thread_I[t], thread_J[t], thread_V[t], raw_bytes, len, row_starts, c_start, c_end, ncols, delim_byte, opts)
+            else
+                resize!(thread_I[t], 0)
+                resize!(thread_J[t], 0)
+                resize!(thread_V[t], 0)
+            end
+        end
+    end
+
+    for t in 1:n_chunks
+        fetch(tasks[t])
     end
 
     total_nnz = sum(length, thread_I)
@@ -261,28 +251,28 @@ function construct_sparse_from_row_major(I::Vector{Int}, J::Vector{Int}, V::Vect
     len_nnz = length(I)
 
     col_counts = zeros(Int, ncols)
-    for k in 1:len_nnz
-        @inbounds col_counts[J[k]] += 1
+    @inbounds for k in 1:len_nnz
+        col_counts[J[k]] += 1
     end
 
     colptr = Vector{Int}(undef, ncols + 1)
     colptr[1] = 1
-    for j in 1:ncols
-        @inbounds colptr[j+1] = colptr[j] + col_counts[j]
+    @inbounds for j in 1:ncols
+        colptr[j + 1] = colptr[j] + col_counts[j]
     end
 
     next_idx = copy(colptr)
     rowval = Vector{Int}(undef, len_nnz)
     nzval = Vector{Float64}(undef, len_nnz)
 
-    for k in 1:len_nnz
-        @inbounds r = I[k]
-        @inbounds c = J[k]
-        @inbounds val = V[k]
-        @inbounds idx = next_idx[c]
-        @inbounds rowval[idx] = r
-        @inbounds nzval[idx] = val
-        @inbounds next_idx[c] += 1
+    @inbounds for k in 1:len_nnz
+        r = I[k]
+        c = J[k]
+        val = V[k]
+        idx = next_idx[c]
+        rowval[idx] = r
+        nzval[idx] = val
+        next_idx[c] += 1
     end
 
     return SparseMatrixCSC(nrows, ncols, colptr, rowval, nzval)
